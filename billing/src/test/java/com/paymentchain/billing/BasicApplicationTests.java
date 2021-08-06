2f1e3159b7fae0b79d71dd58cc7851b781e38c79
Edgar Vega <vayne.edgar271@gmail.com>
2021-08-05 19:02:13 -0500

    add test in java

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -1,16 +1,19 @@
 package com.paymentchain.billing;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-//@SpringBootTest
+@SpringBootTest
 public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		String meessage = "default messafe change test DevOps";
+		Assert.assertNotNull(meessage);
 	}
 
 }
